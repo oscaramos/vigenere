4cf156cf39808de035277decf374c0c95dbc5f76
Oscar <oscar.ramos@ucsp.edu.pe>
2020-06-03 21:43:47 -0500

    Updates

---
+++ precache-manifest.a2fd36530d0330f424d3eca966dce321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaddf7dc0314ddc259a5c0e183388e35",
+    "url": "/vigenere/index.html"
+  },
+  {
+    "revision": "8169aeb6e1bf0f12e19d",
+    "url": "/vigenere/static/js/2.07d54528.chunk.js"
+  },
+  {
+    "revision": "a7b26f0881b89df8ffe608597848bfbb",
+    "url": "/vigenere/static/js/2.07d54528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3419bc92b580e9bd61",
+    "url": "/vigenere/static/js/main.0d1504b9.chunk.js"
+  },
+  {
+    "revision": "d2129cd5372af702ed14",
+    "url": "/vigenere/static/js/runtime-main.32fd4ad8.js"
+  }
+]);
